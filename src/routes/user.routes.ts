034fe69ac8228d031e479e5dad8d3069f47b2a3d
Lucas Santos <lucasa95santos@gmail.com>
2020-11-17 01:51:29 -0400

    chore: configurando a estrutura de rotas

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const usersRouter = Router();
+
+// routes
+// usersRouter.get('/', );
+
+// usersRouter.post('/', );
+
+// usersRouter.put('/', );
+
+// usersRouter.post('/auth', );
+
+export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+// routes
+import routes from './routes';
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+server.use(routes);
 
 const PORT = process.env.PORT || 8080;
 
